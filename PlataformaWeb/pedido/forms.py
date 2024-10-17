5680560213d627ed4f7ad2009444efbc7085768b
Grupo1-Meli <103468726+Grupo1-Meli@users.noreply.github.com>
2024-10-16 21:51:32 -0400

    ecommerce

--- PlataformaWeb/pedido/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Pedido
-
-class PedidoForm(forms.ModelForm):
-    class Meta:
-        model = Pedido
-        fields = ['nombre', 'apellido', 'telefono', 'email', 'direccion_1', 'direccion_2', 'pais', 'ciudad', 'region', 'pedido_nota']
--- PlataformaWeb/tienda/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import RevisarRating
-
-
-class RevisarForm(forms.ModelForm):
-    class Meta:
-        model = RevisarRating
-        fields = ['subject', 'review', 'rating']
