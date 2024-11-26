9b4c1c9667ada3d7ad093707829eea7d6778e95e
enriqueballestero <101352527+enriqueballestero@users.noreply.github.com>
2024-11-25 22:07:09 -0300

    modificaciones de reservaciones

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-#import pymysql
-#pymysql.install_as_MySQLdb()
+import pymysql
+pymysql.install_as_MySQLdb()
 import os
 from pathlib import Path
+
 
 #from decouple import config
 #pip install python-decouple
